e5cfb29f33b03e9e0484ced2f54cc00210ca2fe5
matheusmichalski <matheus.michalskifc@gmail.com>
2026-02-26 08:59:06 -0300

    FEAT: create Category model and update User model options

---
+++ core/models/category.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Category(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'({self.id})-{self.description}'
+
+    class Meta:
+        verbose_name_plural = 'categories'
--- core/models/user.py
+++ core/models/user.py
@@ -60,5 +60,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         """Meta options for the model."""
 
-        verbose_name = 'Usuário'
-        verbose_name_plural = 'Usuários'
+        verbose_name = 'User'
